ed13d07810b0336bc6686e4e38e4c2f8a362ffb4
Misha <stun007@gmail,com>
2017-12-28 01:51:49 +0300

    +

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ color_purple = (255, 0, 255)
 cap = cv2.VideoCapture("03.wmv")
 template = cv2.imread("opponent.bmp")
 fgbg = cv2.createBackgroundSubtractorMOG2()
-while 1:
+i = 0
+while True:
     ret, frame = cap.read()
     if frame is None:
         break
@@ -19,13 +20,15 @@ while 1:
     cv2.medianBlur(fgmask, 9, fgmask)
 
     im2, contours, hierarchy = cv2.findContours(fgmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)  # Поиск контуров
-    cv2.cvtColor(fgmask, cv2.COLOR_GRAY2BGR)
+    # fgmask = cv2.cvtColor(fgmask, cv2.COLOR_GRAY2BGR)
+    # cv2.drawContours(fgmask, contours, -1, color_red, 2, 8, hierarchy)
     point_robot = 0
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)  # Поиск ограничивающего прямоугольника
         if w < 50:
             continue
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (100, 0, 0), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), color_purple, 2)
+        # cv2.rectangle(fgmask, (x, y), (x + w, y + h), color_green, 2)
         point_robot = (x + int(w / 2), y + int(h / 2))
         cv2.circle(frame, point_robot, 3, (0, 0, 255), -1)
     w, h = template.shape[:-1]
@@ -42,10 +45,15 @@ while 1:
         distance = math.sqrt(math.pow(point_robot[0] - point_opponent[0], 2)
                              + math.pow(point_robot[1] - point_opponent[1], 2))
         cv2.putText(frame, str(round(distance, 3)), (0, height - 5), cv2.FONT_HERSHEY_PLAIN, 2, color_yellow, 1)
+    cv2.putText(frame, str(i), (0, 25), cv2.FONT_HERSHEY_PLAIN, 2, color_yellow, 1)
     cv2.imshow('frame', frame)
+    cv2.imshow('fgmask', fgmask)
     k = cv2.waitKey(1) & 0xff
     if k == 27:
+        cv2.imwrite('frame.png', frame)
+        cv2.imwrite('fgmask.png', fgmask)
         break
+    i += 1
 cap.release()
 cv2.destroyAllWindows()
 
